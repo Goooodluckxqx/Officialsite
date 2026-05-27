f66bb412e01b1dfd9283e4ea492a9a563286996a
root <root@localhost.localdomain>
2026-05-27 22:18:07 +0800

    feat: 启用静态导出，修复Vercel npm安装问题

--- next.config.js
+++ next.config.js
@@ -2,6 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  output: 'export', // 启用静态导出
+  images: {
+    unoptimized: true, // 静态导出时需要禁用图片优化
+  },
 }
 
 module.exports = nextConfig
